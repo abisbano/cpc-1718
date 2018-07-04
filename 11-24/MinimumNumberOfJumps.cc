ee42c71787afcf9abc3e0c6429cb023e7e9344dc
Andrea Bisbano <a.bisbano@studenti.unipi.it>
2018-07-05 00:35:12 +0200

    Finalized

--- 11-24/MinimumNumberOfJumps.cc
+++ 11-24/MinimumNumberOfJumps.cc
@@ -4,10 +4,22 @@
  Date: 25/01/18
  Problem: https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps/0
  Solution description:
-
-
- Time cost:
- Space cost:
+  This algorithm uses the support of three values: maxReach, step and jumps.
+  - maxReach represent the maximum reachable index in any moment. It is initialized with the value
+    of the first element of the input array.
+  - step represent the number of steps that can be done before a jump (it is also initalized with
+    the value of the first element of the input array).
+  - jumps represent the minimum number of jumps needed to reach the current index (starts from 1)
+  Then the algorithm loops on the input array and at each step:
+  1. if the current index is the last element it means the end is reachable and the solution
+    is the value of `jumps'.
+  2. it computes the new maximum index reachable, which is the maximum between the current and
+    the last index reachable from the current index.
+  3. decreases the step counter.
+  4. if there aren't steps left, to go to the next index is needed a jump, so it increments a jump,
+    but only if the current index is in the reachable range (e.g. is less than maxReach).
+ Time cost: O(N)
+ Space cost: O(1)
  */
 
 #include<vector>
@@ -15,22 +27,37 @@
 #include<cassert>
 #include<algorithm>
 
-#define INF 1000
+int64_t minimumNumberOfJumps(const std::vector<size_t> &vec, size_t n) {
 
-int64_t minimumNumberOfJumps(const std::vector<uint64_t> &vec) {
-  if (vec.empty()) {
+  if (n <= 1) {
+    return 0;
+  }
+  if (vec[0] == 0) {
     return -1;
   }
-  size_t N = vec.size();
-  std::vector<uint64_t> paths(N,INF);
-  paths[0] = 0;
 
-  for (size_t i = 0; i < N; ++i) {
-    for (size_t j = i + 1; j < N && j <= i + vec[i]; ++j) {
-      paths[j] = std::min(paths[j], paths[i] + 1);
+  size_t maxReach = vec[0]; // maximal reachable index in the array
+  size_t step = vec[0]; //
+  size_t jumps = 1;
+
+  for (size_t i = 1; i < n; ++i) {
+    if (i == n-1) {
+      return jumps;
     }
+    maxReach = std::max(maxReach, i+vec[i]);
+    --step;
+
+    if (step == 0) {
+      ++jumps;
+      if (i >= maxReach) {
+        return -1;
+      }
+      step = maxReach - i;
+    }
+
   }
-  return (paths[N-1] == INF) ? -1 : paths[N-1];
+  return -1;
+
 }
 
 int main() {
@@ -38,8 +65,8 @@ int main() {
   std::cin.tie(NULL);
 
   size_t T, N;
-  uint64_t value;
-  std::vector<uint64_t> vec;
+  size_t value;
+  std::vector<size_t> vec;
 
   std::cin >> T;
   assert(T >= 1 && T <= 40);
@@ -53,7 +80,7 @@ int main() {
       vec.push_back(value);
     }
 
-    std::cout << minimumNumberOfJumps(vec) << "\n";
+    std::cout << minimumNumberOfJumps(vec, N) << "\n";
     vec.clear();
   }
   return 0;
