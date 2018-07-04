55da7e4a189ad787ae7f1be596e8d5b277fee7f2
Andrea Bisbano <a.bisbano@studenti.unipi.it>
2018-07-04 16:03:30 +0200

    Finalized

--- 12-01/VertexCover.cc
+++ 12-01/VertexCover.cc
@@ -1,28 +1,38 @@
 /*
-  VertexCover.cc
-
-  author: Andrea Bisbano
-  date: 26/01/18
-  problem: http://www.spoj.com/problems/PT07X/
-
-  solution description:
-
- FIXME: Clean code and write description of solution
-
+ VertexCover.cc
+ Author: Andrea Bisbano
+ Date: 26/01/18
+ Problem: http://www.spoj.com/problems/PT07X/
+ Solution:
+  This algorithm implements a DP solution for the Vertex Cover problem on trees.
+  The idea is that for each node there are two possibilities:
+  1. The node belongs to the VC, so all the children belong to the cover. Then the solution is
+    the sum of the VC of the children plus 1.
+  2. The node doesn't belong to the VC. In that case all the children must belong to the VC,
+    and thus the solution is the sum of the VC of the children.
+  Exploiting this idea, the algorithm computes the VC for the root in both cases (it is part
+  or it isn't part of the VC) and the minimum between those two values is the solution.
+  To exploit the DP, the algorithm keeps the values of both partial VC for each node, so
+  it doesn't have to compute the same value two times.
+ Time cost: O(N) VC for each node is computed only two times.
+ Space cost:  O(N) to keep the intermediate values.
  */
 
 #include<vector>
 #include<iostream>
 #include<cassert>
 #include<algorithm>
+#include<climits>
 
 class tree {
-
   struct node {
     std::vector<uint64_t> adj;
-    uint64_t cover;
+    int32_t cover[2];
 
-    node() : cover(0) {}
+    node() {
+      cover[0] = -1;
+      cover[1] = -1;
+    }
   };
 
   std::vector<node> nodes;
@@ -35,108 +45,53 @@ public:
 
   void addEdge(size_t u, size_t v) {
     nodes[u].adj.push_back(v);
-//    nodes[v].adj.push_back(u);
+    nodes[v].adj.push_back(u);
   }
 
-  uint64_t vertexCover(size_t root, int64_t dad) {
-//    printf("start node: %zu, cover: %llu, adj: %zu\n", root, nodes[root].cover, nodes[root].adj.size());
-//    std::cout << "start node " << root << "\n";
-    if (nodes[root].cover != 0 || nodes[root].adj.empty()){
-//      printf("finished %zu\n", root);
-      return nodes[root].cover;
+  uint64_t vertexCover(size_t root, size_t taken, int32_t dad) {
+    if (nodes[root].cover[taken] != -1) {
+      return nodes[root].cover[taken];
     }
 
     size_t sons = nodes[root].adj.size();
+    uint64_t cover = taken;
 
-    uint64_t sizeIncluded = 1;
     for (size_t i = 0; i < sons; ++i) {
-      if (nodes[root].adj[i] == dad) continue;
-      sizeIncluded += vertexCover(nodes[root].adj[i], root);
-    }
-
-    uint64_t sizeExcluded = 0;
-    for (size_t i = 0; i < sons; ++i) {
-      if (nodes[root].adj[i] == dad) continue;
-      ++sizeExcluded;
-      size_t nephews = nodes[nodes[root].adj[i]].adj.size();
-      for (size_t j = 0; j < nephews; ++j) {
-        if (nodes[nodes[root].adj[i]].adj[j] == root) continue;
-        sizeExcluded += vertexCover(nodes[nodes[root].adj[i]].adj[j], nodes[root].adj[i]);
+      size_t nextSon = nodes[root].adj[i];
+      if (nextSon == dad) continue;
+      if (taken) {
+        cover += std::min(vertexCover(nextSon, 0, root),vertexCover(nextSon, 1, root));
+      } else {
+        cover += vertexCover(nextSon, 1, root);
       }
     }
-//    printf("n: %zu, ex: %llu, inc: %llu\n",root, sizeIncluded, sizeExcluded);
-    nodes[root].cover = std::min(sizeExcluded, sizeIncluded);
-    return  nodes[root].cover;
 
+    nodes[root].cover[taken] = cover;
+    return cover;
   }
 
   uint64_t minVertexCover() {
     if (size == 0) return 0;
-    uint64_t result = 1000000;
-    for (size_t i = 0; i < size; ++i) {
-      if (nodes[i].cover == 0) {
-        vertexCover(i, -1);
-      }
-      if (nodes[i].cover > 0 && nodes[i].cover < result) {
-        result = nodes[i].cover;
-      }
-    }
-    return result;
+    return std::min(vertexCover(0, 0, -1), vertexCover(0,1,-1));
   }
 
 };
-
-//uint64_t vertexCover(std::vector<node> &tree, size_t root, int father) {
-////  std::cout << root << "\n";
-//
-//  if (tree[root].cover != 0)
-//    return tree[root].cover;
-//
-//  if (tree[root].adj.empty()) {
-//    return 0;
-//  }
-//
-//  size_t sons = tree[root].adj.size();
-//
-//  uint64_t sizeIncluded = 1;
-//  for (size_t i = 0; i < sons; ++i) {
-//    if (tree[root].adj[i] != father) {
-//      sizeIncluded += vertexCover(tree, tree[root].adj[i], root);
-//    }
-//  }
-//
-//  uint64_t sizeExcluded = sons;
-//  for (size_t i = 0; i < sons; ++i) {
-//    if (tree[root].adj[i] != father) {
-//      size_t nephews = tree[tree[root].adj[i]].adj.size();
-//      for (size_t j = 0; j < nephews; ++j) {
-//        if (tree[i].adj[j] != root) {
-//          sizeExcluded += vertexCover(tree, tree[i].adj[j], i);
-//        }
-//      }
-//    }
-//  }
-//
-//  tree[root].cover = std::min(sizeExcluded, sizeIncluded);
-//  return tree[root].cover;
-//}
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
 
-  int64_t N;
+  size_t N;
   size_t u, v;
 
   std::cin >> N;
-  assert(N >= 0 && N <= 100000);
+  assert(N <= 100000);
   tree graph(N);
 
-  for (int64_t i = 0; i < N-1; ++i) {
+  for (size_t i = 0; i < N-1; ++i) {
     std::cin >> u >> v;
     assert(u >= 1 && u <= N);
     assert(v >= 1 && v <= N);
-
     graph.addEdge(u-1,v-1);
   }
 
