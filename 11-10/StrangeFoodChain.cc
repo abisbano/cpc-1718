51502138ce63253c1fb81c402efdcbd2f733bc88
Andrea Bisbano <a.bisbano@studenti.unipi.it>
2018-07-05 15:12:57 +0200

    Finalized

--- 11-10/StrangeFoodChain.cc
+++ 11-10/StrangeFoodChain.cc
@@ -1,13 +1,13 @@
 /*
-
  StrangeFoodChain.cc
-
- author: Andrea Bisbano
- date: 17/01/18
- problem: http://www.spoj.com/problems/CHAIN/
-
- solution description:
-  TO BE WRITTEN
+ Author: Andrea Bisbano
+ Date: 17/01/18
+ Problem: http://www.spoj.com/problems/CHAIN/
+ Solution:
+  This algorithm is an implementation of the following solution:
+  https://www.quora.com/How-do-I-solve-the-strange-food-chain-question-on-SPOJ
+ Time cost:
+ Space cost:
  */
 
 #include <iostream>
@@ -15,89 +15,67 @@
 #include <cassert>
 #include <algorithm>
 
-struct node {
-  uint64_t father;
-  uint64_t rank;
-  node (uint64_t f) : father(f), rank(0) { }
-};
-
-struct edge {
-  uint64_t first;
-  uint64_t second;
-  uint64_t weight;
-  edge(uint64_t u, uint64_t v, uint64_t w) : first(u), second(v), weight(w) { }
-};
-
-uint64_t findSet(std::vector<node> &nodes, uint64_t x) {
-  if (x != nodes[x].father)
-    nodes[x].father = findSet(nodes, nodes[x].father);
-  return nodes[x].father;
-}
-
-void link(std::vector<node> &nodes, uint64_t u, uint64_t v) {
-  if (nodes[u].rank > nodes[v].rank) {
-    nodes[v].father = u;
-  } else {
-    nodes[u].father = v;
-    if (nodes[u].rank == nodes[v].rank) {
-      ++nodes[v].rank;
-    }
+int find(std::vector<int> &root,
+         std::vector<int> &d, int v) {
+  if (v == root[v]) {
+    return v;
   }
-}
-
-void unionSet(std::vector<node> &nodes, uint64_t u, uint64_t v) {
-  link(nodes, findSet(nodes, u), findSet(nodes, v));
-}
-
-uint64_t MST(std::vector<edge> &edges, size_t N) {
-  std::vector<node> nodes;
-  nodes.reserve(N);
-
-  uint64_t result = 0;
-  uint64_t counter = 0;
-
-  for (size_t i = 0; i < N; ++i) {
-    nodes.emplace_back(i);
-  }
-
-  std::sort(edges.begin(), edges.end(),
-            [] (edge &a, edge &b) {
-              return a.weight < b.weight;
-            });
-
-  for (auto e : edges) {
-    if (findSet(nodes, e.first) != findSet(nodes, e.second)) {
-      result += e.weight;
-      ++counter;
-      if (counter == N-1) {
-        return result;
-      }
-      unionSet(nodes, e.first, e.second);
-    }
-  }
-  return 0;
+  int tmp = root[v];
+  root[v] = find(root, d, root[v]);
+  d[v] += d[tmp];
+  return root[v];
 }
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
 
-  size_t N, M;
-  uint64_t u, v, w;
-  std::vector<edge> edges;
+  int T; // test cases
+  int N, K;
+  int f, x, y;
+  int answer;
+  std::vector<int> d;
+  std::vector<int> root;
 
-  std::cin >> N >> M;
-  assert(N >= 1 && N <= 10000);
-  assert(M >= 1 && M <= 100000);
-  edges.reserve(M);
-  for (size_t i = 0; i < M; ++i) {
-    std::cin >> u >> v >> w;
-    assert(u >= 1 && u <= N);
-    assert(v >= 1 && v <= N);
-    assert(w >= 0 && w <= 1000000);
-    edges.emplace_back(u-1,v-1,w);
+  std::cin >> T;
+  for (int i = 0; i < T; ++i) {
+    std::cin >> N >> K;
+    assert(N >= 1 && N <= 50000);
+    assert(K >= 1 && K <= 100000);
+    root.resize(N+1);
+    d.resize(N+1,0);
+    for (int j = 0; j < N+1; ++j) {
+      root[j] = j;
+    }
+    answer = 0;
+
+    for (int j = 0; j < K; ++j) {
+      std::cin >> f >> x >> y;
+      // false case 1
+      if (x > N || y > N) {
+        ++answer;
+        continue;
+      }
+      int px = find(root, d, x);
+      int py = find(root, d, y);
+
+      --f;
+
+      if (px == py) {
+        int tmp = (d[x] - d[y]) % 3;
+        if (tmp < 0) tmp += 3;
+        if (tmp != f) {
+          ++answer;
+        }
+      } else {
+        root[px] = py;
+        int h = (d[x] - d[y] - f) % 3;
+        d[px] = h < 0 ? -h : -h + 3;
+      }
+    }
+    std::cout << answer << "\n";
+    root.clear();
+    d.clear();
   }
-
-  std::cout << MST(edges, N) << "\n";
   return 0;
 }
